b3356280ce24c2cd6292a7ea9d7f601283646709
David Rother <david@edv-drucksysteme.de>
2022-11-23 08:40:58 +0100

    stuff

--- interaction_learning/core/util.py
+++ interaction_learning/core/util.py
@@ -2,6 +2,8 @@ import copy
 import random
 import numpy as np
 import torch
+import math
+from math import pi
 
 
 def make_deterministic(random_seed):
@@ -26,4 +28,24 @@ class AgentPositionGenerator:
 
     def reset(self):
         self.pointer = 0
+
+
+class AgentPositionGeneratorCircle:
+
+    def __init__(self, num_pos, num_agents=2, position_encodings=None):
+        self.agent_positions = [[] for enc in position_encodings] for _ in range(num_pos)]
+        self.pointer = 0
+
+    def __call__(self, *args, **kwargs):
+        pos = self.agent_positions[self.pointer]
+        self.pointer += 1
+        return copy.deepcopy(pos)
+
+    def reset(self):
+        self.pointer = 0
+
+    @staticmethod
+    def points_on_circumference(center=(0, 0), r=50, n=100):
+        return [(center[0] + (math.cos(2 * pi / n * x) * r),  center[1] + (math.sin(2 * pi / n * x) * r))
+                for x in range(0, n + 1)]
 
